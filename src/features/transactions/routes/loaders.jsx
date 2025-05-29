ccecbea6020355a11417a3b80766b765c6d96534
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2025-05-29 11:52:02 +0300

    feat: utilize updated services for both loaders

--- src/features/transactions/routes/loaders.jsx
+++ src/features/transactions/routes/loaders.jsx
@@ -1,7 +1,39 @@
 import { account } from "../../../lib/appwrite/client";
+import { transactionService } from "../services/transaction.service";
+import { parseTransactionParams } from "../utils/url-params";
+import { currencyService } from "../../currencies/services/currency.service";
+import { balancesService } from "../../balances/services";
 
-export async function transactionsLoader() {}
+export async function transactionsLoader({ request }) {
+  const { $id: userId } = await account.get();
+  const url = new URL(request.url);
+  const params = parseTransactionParams(url.searchParams);
 
-export async function transactionLoader() {}
+  const transactionsPromise = transactionService.listTransactions(params);
+  const currentMonthSummaryPromise = balancesService.getCurrentMonthSummary();
+  const currencyPreferencePromise = currencyService.getPreferences(userId);
 
-export async function editTransactionLoader() {}
+  return {
+    transactions: transactionsPromise,
+    currentMonthSummary: currentMonthSummaryPromise,
+    currencyPreferences: currencyPreferencePromise,
+  };
+}
+
+export async function transactionLoader({ params }) {
+  try {
+    const { $id: userId } = await account.get();
+    const transaction = transactionService.getTransaction(params.id);
+    const currencyPreference = currencyService.getPreferredCurrency(userId);
+
+    const data = Promise.all([transaction, currencyPreference]);
+
+    return { data };
+  } catch (error) {
+    return {
+      error: true,
+      message:
+        error.message || "An error occurred while loading the transaction.",
+    };
+  }
+}
