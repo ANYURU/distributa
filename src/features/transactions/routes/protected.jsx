03bd084fae1d27c9345943028c15b829900b88c0
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2025-05-29 11:53:18 +0300

    feat: add loaders, actions and elements to the data route

--- src/features/transactions/routes/protected.jsx
+++ src/features/transactions/routes/protected.jsx
@@ -1,17 +1,32 @@
+import { transactionsLoader, transactionLoader } from "./loaders";
+import {
+  createTransactionAction,
+  updateTransactionAction,
+  deleteTransactionAction,
+} from "./actions";
+import { Transactions, Transaction } from "../../../pages";
+
 export default [
   {
     path: "/transactions",
+    loader: transactionsLoader,
+    element: <Transactions />,
   },
   {
     path: "/transactions/new",
+    action: createTransactionAction,
   },
   {
     path: "/transactions/:id",
+    loader: transactionLoader,
+    element: <Transaction />,
   },
   {
     path: "/transactions/:id/edit",
+    action: updateTransactionAction
   },
   {
     path: "/transactions/:id/delete",
+    action: deleteTransactionAction
   },
 ];
