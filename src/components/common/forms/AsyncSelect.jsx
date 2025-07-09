faee4a77b9568922535c7ce68a4b2ce1031a29a3
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2025-07-09 18:58:38 +0300

    feat: optimize the async select

--- src/components/common/forms/AsyncSelect.jsx
+++ src/components/common/forms/AsyncSelect.jsx
@@ -3,170 +3,172 @@ import AsyncSelect from "react-select/async";
 import clsx from "clsx";
 import { selectStyles } from "./selectStyles";
 import { DropdownIndicator, LoadingMessage } from "./selectComponents";
-import { useEffect, useState } from "react";
+import { useEffect, useState, memo } from "react";
 import Button from "./Button";
 import { FiPlus } from "react-icons/fi";
 
-const CustomAsyncSelect = ({
-  label,
-  loadOptions,
-  defaultOptions = true,
-  cacheOptions = true,
-  disabled,
-  handleChange,
-  onCreateOption = null,
-  isClearable = false,
-  isSearchable = true,
-  placeholder = "Search...",
-  debounceTimeout = 300,
-  getOptionValue = (option) => option.value,
-  ...props
-}) => {
-  const [field, meta, helpers] = useField(props);
-  const { error, touched } = meta;
-  const { setValue } = helpers;
-  const [selectedOption, setSelectedOption] = useState(null);
+const CustomAsyncSelect = memo(
+  ({
+    label,
+    loadOptions,
+    defaultOptions = true,
+    cacheOptions = true,
+    disabled,
+    handleChange,
+    onCreateOption = null,
+    isClearable = false,
+    isSearchable = true,
+    placeholder = "Search...",
+    debounceTimeout = 300,
+    getOptionValue = (option) => option.value,
+    ...props
+  }) => {
+    const [field, meta, helpers] = useField(props);
+    const { error, touched } = meta;
+    const { setValue } = helpers;
+    const [selectedOption, setSelectedOption] = useState(null);
 
-  useEffect(() => {
-    const loadInitialValue = async () => {
-      if (field.value && loadOptions) {
-        try {
-          const options = await loadOptions("");
+    useEffect(() => {
+      const loadInitialValue = async () => {
+        if (field.value && loadOptions) {
+          try {
+            const options = await loadOptions("");
 
-          const currentOption = options.find(
-            (option) => getOptionValue(option) === field.value
-          );
+            const currentOption = options.find(
+              (option) => getOptionValue(option) === field.value
+            );
 
-          if (currentOption) {
-            setSelectedOption(currentOption);
+            if (currentOption) {
+              setSelectedOption(currentOption);
+            }
+          } catch (error) {
+            console.error("Error loading initial value:", error);
           }
-        } catch (error) {
-          console.error("Error loading initial value:", error);
         }
+      };
+
+      loadInitialValue();
+    }, [field.value, loadOptions, getOptionValue]);
+
+    // Custom onChange handler that updates both the form value and the selected option
+    const onChangeHandler = (selected) => {
+      setSelectedOption(selected);
+
+      if (handleChange) {
+        handleChange(selected);
+      } else {
+        // Default behavior if no custom handler is provided
+        setValue(selected ? getOptionValue(selected) : "");
       }
     };
 
-    loadInitialValue();
-  }, [field.value, loadOptions, getOptionValue]);
+    const {
+      placeholderStyles,
+      selectInputStyles,
+      valueContainerStyles,
+      singleValueStyles,
+      multiValueStyles,
+      multiValueLabelStyles,
+      multiValueRemoveStyles,
+      indicatorsContainerStyles,
+      clearIndicatorStyles,
+      indicatorSeparatorStyles,
+      dropdownIndicatorStyles,
+      menuStyles,
+      menuListStyles,
+      groupHeadingStyles,
+      optionStyles,
+      controlStyles,
+      noOptionsMessageStyles,
+      reactSelectStyles,
+    } = selectStyles;
 
-  // Custom onChange handler that updates both the form value and the selected option
-  const onChangeHandler = (selected) => {
-    setSelectedOption(selected);
-
-    if (handleChange) {
-      handleChange(selected);
-    } else {
-      // Default behavior if no custom handler is provided
-      setValue(selected ? getOptionValue(selected) : "");
-    }
-  };
-
-  const {
-    placeholderStyles,
-    selectInputStyles,
-    valueContainerStyles,
-    singleValueStyles,
-    multiValueStyles,
-    multiValueLabelStyles,
-    multiValueRemoveStyles,
-    indicatorsContainerStyles,
-    clearIndicatorStyles,
-    indicatorSeparatorStyles,
-    dropdownIndicatorStyles,
-    menuStyles,
-    menuListStyles,
-    groupHeadingStyles,
-    optionStyles,
-    controlStyles,
-    noOptionsMessageStyles,
-    reactSelectStyles,
-  } = selectStyles;
-
-  return (
-    <div className="w-full flex flex-col gap-y-2">
-      {label ? (
-        <label
-          className="font-satoshi font-normal text-small leading-100 tracking-normal"
-          htmlFor={props.name}
-        >
-          {label}
-        </label>
-      ) : null}
-      <div className="flex flex-row h-fit">
-        <div className="w-full">
-          <AsyncSelect
-            {...field}
-            onChange={onChangeHandler}
-            loadOptions={loadOptions}
-            defaultOptions={defaultOptions}
-            cacheOptions={cacheOptions}
-            closeMenuOnSelect={true}
-            isClearable={isClearable}
-            isDisabled={disabled}
-            isSearchable={isSearchable}
-            debounceTimeout={debounceTimeout}
-            getOptionValue={getOptionValue}
-            value={selectedOption}
-            unstyled
-            placeholder={placeholder}
-            styles={reactSelectStyles}
-            components={{
-              DropdownIndicator,
-              LoadingMessage,
-            }}
-            classNames={{
-              control: ({ isFocused }) =>
-                clsx(
-                  isFocused ? controlStyles.focus : controlStyles.nonFocus,
-                  controlStyles.base,
-                  meta.touched && meta.error
-                    ? "border border-error focus:border-error"
-                    : "border border-greyborder focus:border-grey"
-                ),
-              menuStyles: () => menuStyles,
-              menuList: () => menuListStyles,
-              placeholder: () => placeholderStyles,
-              input: () => selectInputStyles,
-              valueContainer: () => valueContainerStyles,
-              singleValue: () => singleValueStyles,
-              multiValue: () => multiValueStyles,
-              multiValueLabel: () => multiValueLabelStyles,
-              multiValueRemove: () => multiValueRemoveStyles,
-              indicatorsContainer: () => indicatorsContainerStyles,
-              clearIndicator: () => clearIndicatorStyles,
-              indicatorSeparator: () => indicatorSeparatorStyles,
-              dropdownIndicator: () => dropdownIndicatorStyles,
-              menu: () => menuStyles,
-              groupHeading: () => groupHeadingStyles,
-              option: ({ isFocused, isSelected }) =>
-                clsx(
-                  isFocused && optionStyles.focus,
-                  isSelected && optionStyles.selected,
-                  optionStyles.base
-                ),
-              noOptionsMessage: () => noOptionsMessageStyles,
-            }}
-          />
-        </div>
-        {onCreateOption && (
-          <Button
-            type="button"
-            className="font-medium p-3 h-full align-middle border-transparent text-black"
-            onClick={onCreateOption}
-            kind="plain"
+    return (
+      <div className="w-full flex flex-col gap-y-2">
+        {label ? (
+          <label
+            className="font-satoshi font-normal text-small leading-100 tracking-normal"
+            htmlFor={props.name}
           >
-            <FiPlus />
-          </Button>
-        )}
-      </div>
-
-      {touched && error ? (
-        <div className="font-normal font-satoshi text-tiny tracking-normal leading-150 text-error">
-          {meta.error}
+            {label}
+          </label>
+        ) : null}
+        <div className="flex flex-row h-fit">
+          <div className="w-full">
+            <AsyncSelect
+              {...field}
+              onChange={onChangeHandler}
+              loadOptions={loadOptions}
+              defaultOptions={defaultOptions}
+              cacheOptions={cacheOptions}
+              closeMenuOnSelect={true}
+              isClearable={isClearable}
+              isDisabled={disabled}
+              isSearchable={isSearchable}
+              debounceTimeout={debounceTimeout}
+              getOptionValue={getOptionValue}
+              value={selectedOption}
+              unstyled
+              placeholder={placeholder}
+              styles={reactSelectStyles}
+              components={{
+                DropdownIndicator,
+                LoadingMessage,
+              }}
+              classNames={{
+                control: ({ isFocused }) =>
+                  clsx(
+                    isFocused ? controlStyles.focus : controlStyles.nonFocus,
+                    controlStyles.base,
+                    meta.touched && meta.error
+                      ? "border border-error focus:border-error"
+                      : "border border-greyborder focus:border-grey"
+                  ),
+                menuStyles: () => menuStyles,
+                menuList: () => menuListStyles,
+                placeholder: () => placeholderStyles,
+                input: () => selectInputStyles,
+                valueContainer: () => valueContainerStyles,
+                singleValue: () => singleValueStyles,
+                multiValue: () => multiValueStyles,
+                multiValueLabel: () => multiValueLabelStyles,
+                multiValueRemove: () => multiValueRemoveStyles,
+                indicatorsContainer: () => indicatorsContainerStyles,
+                clearIndicator: () => clearIndicatorStyles,
+                indicatorSeparator: () => indicatorSeparatorStyles,
+                dropdownIndicator: () => dropdownIndicatorStyles,
+                menu: () => menuStyles,
+                groupHeading: () => groupHeadingStyles,
+                option: ({ isFocused, isSelected }) =>
+                  clsx(
+                    isFocused && optionStyles.focus,
+                    isSelected && optionStyles.selected,
+                    optionStyles.base
+                  ),
+                noOptionsMessage: () => noOptionsMessageStyles,
+              }}
+            />
+          </div>
+          {onCreateOption && (
+            <Button
+              type="button"
+              className="font-medium p-3 h-full align-middle border-transparent text-black"
+              onClick={onCreateOption}
+              kind="plain"
+            >
+              <FiPlus />
+            </Button>
+          )}
         </div>
-      ) : null}
-    </div>
-  );
-};
+
+        {touched && error ? (
+          <div className="font-normal font-satoshi text-tiny tracking-normal leading-150 text-error">
+            {meta.error}
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+);
 
 export default CustomAsyncSelect;
